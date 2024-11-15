068e47bf97dc85da5e07cccacf08ab9efd190641
yevhenyelisieiev <yevhenyelisieiev@gmail.com>
2024-11-15 12:10:43 -0700

    Added Odd_or_Even task solution

--- src/eugene/week_1/OddOrEven.java
+++ src/eugene/week_1/OddOrEven.java
@@ -1,7 +1,35 @@
 package eugene.week_1;
 
+import java.util.Scanner;
+
 public class OddOrEven {
 
+
+    //Created a method:
+        public static boolean isEven(int number) {
+            return number % 2 == 0;
+        }
+
+
+    //The code to check is Odd or Even:
+        public static void main(String[] args) {
+
+    //Getting a number using a Scanner:
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+    //Checked if given number is Odd or Even calling an "isEven()" method:
+        if (isEven(number)) {
+            System.out.println("identify(" + number + ") ->" + " \"Even\"");
+        } else {
+            System.out.println("identify(" + number + ") ->" + " \"Odd\"");
+
+        }
+
+
+    }
 }
 
 
@@ -10,13 +38,5 @@ public class OddOrEven {
 EX:
 identify(5) ->  "Odd"
 identify(6) ->  "Even"
-
-
-
-Added more modules
-
-This is new day and more job needs to be done
-
      */
 
-//working on the code
