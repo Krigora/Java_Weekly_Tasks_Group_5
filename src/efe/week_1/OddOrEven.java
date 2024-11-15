4eab117272c58e7a266526f00f433ade5f21238a
GNefe <efegunes2012@icloud.com>
2024-11-15 12:23:45 -0500

    Cloned the project Added changes

---
+++ src/efe/week_1/OddOrEven.java
@@ -0,0 +1,13 @@
+package efe.week_1;
+
+public class OddOrEven {
+//some changes
+
+}
+/*
+    Write  a method which can identifies given number is even or odd
+EX:
+identify(5) ->  "Odd"
+identify(6) ->  "Even"
+
+     */
