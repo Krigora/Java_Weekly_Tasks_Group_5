81e577f733ca2b9d67d92e4fb5f5dc53f1366152
alinaaminova <alinaaminova85@gmail.com>
2024-11-15 12:13:56 -0500

    Created my package for week1

---
+++ src/alina/week_1/OddOrEven.java
@@ -0,0 +1,12 @@
+package alina.week_1;
+
+public class OddOrEven {
+
+    /*
+    Write  a method which can identifies given number is even or odd
+EX:
+identify(5) ->  "Odd"
+identify(6) ->  "Even"
+
+     */
+}
