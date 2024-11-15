6adde6eeb3e887efb6e609c8b5727ac3ca4e100d
ihorkryvynskyy <NEWlife20!>
2024-11-15 11:41:11 -0600

    updated some information

--- src/ihor/week_1/OddOrEven.java
+++ src/ihor/week_1/OddOrEven.java
@@ -1,16 +1,8 @@
 package ihor.week_1;
 
 public class OddOrEven {
-    public static void main(String[] args) {
-        int num = 5;
 
-        if(num % 2 == 0){
-            System.out.println(num + " is even ");
-        }else{
-            System.out.println(num + " is odd ");
-        }
-    }
-
+// add some information
 }
 
 
