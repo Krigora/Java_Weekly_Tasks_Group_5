d4c64212f0fcb03650d6658461f326247bada2a0
ihorkryvynskyy <NEWlife20!>
2024-11-15 10:55:44 -0600

    added first code

--- src/ihor/week_1/OddOrEven.java
+++ src/ihor/week_1/OddOrEven.java
@@ -1,9 +1,18 @@
 package ihor.week_1;
 
 public class OddOrEven {
-//fddffs
+    public static void main(String[] args) {
+        int num = 5;
 
+        if(num % 2 == 0){
+            System.out.println(num + " is even ");
+        }else{
+            System.out.println(num + " is odd ");
+        }
     }
+
+}
+
 
 /*
     Write  a method which can identifies given number is even or odd
