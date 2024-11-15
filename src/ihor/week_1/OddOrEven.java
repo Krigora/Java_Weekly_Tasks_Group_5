03aa1612310edc937ca127e4967ed56d7683acec
ihorkryvynskyy <NEWlife20!>
2024-11-15 12:11:35 -0600

    solution for odd or Even

--- src/ihor/week_1/OddOrEven.java
+++ src/ihor/week_1/OddOrEven.java
@@ -1,9 +1,18 @@
 package ihor.week_1;
 
 public class OddOrEven {
+    public static void main(String[] args) {
+        int num = 6;
 
-// add some information
-}
+        if(num % 2 == 0){
+            System.out.println(num + " is even ");
+        }else{
+            System.out.println(num + " is odd ");
+        }
+    }
+    }
+
+
 
 
 /*
