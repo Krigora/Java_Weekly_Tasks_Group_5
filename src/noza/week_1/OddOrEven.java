aecf5073f48a9bb96930970ac6a248ecb4b30c1f
NozaM5 <mnoza85@gmail.com>
2024-11-18 21:26:26 -0600

    Updated the OddOrEven class with the task solution

--- src/noza/week_1/OddOrEven.java
+++ src/noza/week_1/OddOrEven.java
@@ -10,6 +10,20 @@ public class OddOrEven {
      */
 
 
+    public static void main(String[] args) {
+
+        //calling the identify() method:
+        System.out.println(identify(6)); //Output: Even
+
+
+    }
+
+
+    //this method returns a string and identifies if the given number is even or odd
+    public static String identify(int num){
+        return (num % 2 == 0) ? "Even" : "Odd";
+    }
+
 
 
 
