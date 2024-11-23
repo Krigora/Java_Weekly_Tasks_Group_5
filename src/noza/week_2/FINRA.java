2e049f317bc9f9e8293edb820a1d132c691a0832
NozaM5 <mnoza85@gmail.com>
2024-11-23 17:13:00 -0600

    Added the task solution to FINRA class

--- src/noza/week_2/FINRA.java
+++ src/noza/week_2/FINRA.java
@@ -14,6 +14,47 @@ public class FINRA {
 
 
 
+    public static void main(String[] args) {
+
+        //calling printNumbers() method
+        printNumbers(); // Output:
+        // 1 2 FIN 4 RA FIN 7 8 FIN RA 11 FIN 13 14 FINRA 16 17 FIN 19 RA FIN 22 23 FIN RA 26 FIN 28 29 FINRA
+
+    }
+
+
+
+    //this method prints out the numbers from 1 to 30 following the FINRA rules
+    public static void printNumbers(){
+
+        //loop from 1 to 30 (inclusive)
+        for (int i = 1; i <= 30; i++) {
+
+            //declaring boolean local variables to store nums divisible by 3:
+            boolean by3 = i % 3 == 0;
+
+            //nums divisible by 5:
+            boolean by5 = i % 5 == 0;
+
+
+            // First check if the number is divisible by both 3 and 5
+            if (by3 && by5) {
+                System.out.print("FINRA ");
+            } else if (by3) {
+                System.out.print("FIN ");
+            } else if (by5) {
+                System.out.print("RA ");
+            } else {
+                System.out.print(i + " ");
+            }
+
+
+        }
+
+
+
+    }
+
 
 
 
