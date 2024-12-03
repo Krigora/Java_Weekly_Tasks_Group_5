f1d397ad851bb7ca47a6a15967ffe0e91f04032a
youness <younessabouelouafa89@gmail.com>
2024-12-03 08:00:51 -0500

    created class Swaping two numbers

---
+++ src/youness/week_3/SwapTwoNumbers.java
@@ -0,0 +1,21 @@
+package youness.week_3;
+
+public class SwapTwoNumbers {
+     /*
+    Swap 2 numbers without using a 3rd variable
+     */
+     public static void main(String[] args) {
+         int x= 20;
+         int y= 10;
+         System.out.println("Before Swaping , x = " + x+ " and y = " + y);
+
+         x= x+y;
+         y= x-y;
+         x= x-y;
+
+         System.out.println("Afetr Swaping , x = " + x+ " and y = " + y);
+
+
+     }
+
+}
