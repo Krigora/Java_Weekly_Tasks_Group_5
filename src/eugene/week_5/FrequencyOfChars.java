da493e7dc8f6f0ee712f0b7884eee87114358ecd
yevhenyelisieiev <yevhenyelisieiev@gmail.com>
2024-12-13 10:36:21 -0500

    Updating week5 task

--- src/eugene/week_5/FrequencyOfChars.java
+++ src/eugene/week_5/FrequencyOfChars.java
@@ -2,3 +2,9 @@ package eugene.week_5;
 
 public class FrequencyOfChars {
 }
+
+/*
+Frequency of Characters
+Write a return method that can find the frequency of characters
+Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+ */
