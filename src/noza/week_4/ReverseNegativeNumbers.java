8861525cd6cbac67b9a5844626dd234fe59db799
NozaM5 <mnoza85@gmail.com>
2024-12-18 18:32:57 -0600

    Created week_4 package with ReverseNegativeNumbers class and week_5 package with FrequencyOfChars class

---
+++ src/noza/week_4/ReverseNegativeNumbers.java
@@ -0,0 +1,21 @@
+package noza.week_4;
+
+public class ReverseNegativeNumbers {
+
+
+    /*
+    `2. Numbers -- Reverse negative number
+Write a return method that can reverse
+negative number and return it as int
+     */
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/noza/week_5/FrequencyOfChars.java
@@ -0,0 +1,18 @@
+package noza.week_5;
+
+public class FrequencyOfChars {
+
+
+    /*
+    `Frequency of Characters
+Write a return method that can find the frequency of characters
+Ex:  FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+     */
+
+
+
+
+
+
+
+}
