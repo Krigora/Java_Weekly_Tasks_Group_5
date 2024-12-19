5de81204448b945a885f7a8537e6656be1fb75b8
NozaM5 <mnoza85@gmail.com>
2024-12-18 18:34:01 -0600

    Created week_6 package with RemoveDuplicates class

---
+++ src/noza/week_6/RemoveDuplicates.java
@@ -0,0 +1,17 @@
+package noza.week_6;
+
+public class RemoveDuplicates {
+
+
+    /*
+    `Write a return method that can remove the duplicated values from String
+Ex:  removeDup("AAABBBCCC")  ==> ABC
+     */
+
+
+
+
+
+
+
+}
