c19d6fb35faf28731a9789c5aebd0b43c2734e97
yevhenyelisieiev <yevhenyelisieiev@gmail.com>
2025-01-10 00:08:38 -0700

    Added week07 weekly java task

---
+++ src/eugene/week_7/FindTheUniqueCharacter.java
@@ -0,0 +1,13 @@
+package eugene.week_7;
+
+public class FindTheUniqueCharacter {
+
+
+}
+
+
+/*
+`String -- Find the unique
+Write a return method that can find the unique characters from the String
+Ex:  unique("AAABBBCCCDEF")  ==>  "DEF";
+ */
