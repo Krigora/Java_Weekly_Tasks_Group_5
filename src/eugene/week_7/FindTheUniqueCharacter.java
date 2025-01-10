b96694fccb5a1690b666fe9869c1b6654de12991
yevhenyelisieiev <yevhenyelisieiev@gmail.com>
2025-01-10 09:20:23 -0700

    Added week07 task solution

--- src/eugene/week_7/FindTheUniqueCharacter.java
+++ src/eugene/week_7/FindTheUniqueCharacter.java
@@ -2,8 +2,30 @@ package eugene.week_7;
 
 public class FindTheUniqueCharacter {
 
+        public static String findUnique(String str) {
 
-}
+            String unique = "";
+
+            for (int i = 0; i < str.length(); i++) {
+                char ch = str.charAt(i);
+                // Check if the character appears only once in the string
+                if (str.indexOf(ch) == str.lastIndexOf(ch)) {
+                    unique += ch; // Append the unique character
+                }
+            }
+
+            return unique;
+        }
+
+        public static void main(String[] args) {
+            String input = "AAABBBCCCDEF";
+            String result = findUnique(input);
+            System.out.println("Unique characters: " + result);
+        }
+    }
+
+
+
 
 
 /*
