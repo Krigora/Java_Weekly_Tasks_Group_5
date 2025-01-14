30f72631f77ce34b551ab2736f5609caf5927af7
yevhenyelisieiev <yevhenyelisieiev@gmail.com>
2025-01-14 13:40:00 -0700

    Added week08 task solution

--- src/eugene/week_8/StringReverse.java
+++ src/eugene/week_8/StringReverse.java
@@ -1,7 +1,27 @@
 package eugene.week_8;
 
+import java.util.Scanner;
+
 public class StringReverse {
+    public static String reverseString(String str) {
+        String reversedStr = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversedStr += str.charAt(i);
+        }
+        return reversedStr;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a string: ");
+        String inputString = scanner.nextLine();
+        String reversedString = reverseString(inputString);
+        System.out.println("Reversed string: " + reversedString);
+        scanner.close();
+    }
 }
+
+
 
 
 /*
